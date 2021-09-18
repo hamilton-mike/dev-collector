d8c7331849d308d5d739c1e19a1df8169899c820
Michael Hamilton <hamiltonisaiah9119@gmail.com>
2021-09-17 22:10:34 -0700

    render data in dtl

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('devs/', views.devs_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Dev:
+    def __init__(self, name, location, age, remote):
+        self.name = name
+        self.location = location
+        self.age = age
+        self.remote = remote
+
+devs = [
+    Dev('Ryan', 'Las Vegas, Nv', 21, True),
+    Dev('Jamie', 'Sacramento, Ca', 41, False),
+    Dev('Curtis', 'Phoenix, Az', 30, True)
+]
+
 def home(request):
     return HttpResponse('<h1>Hello World!</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def devs_index(request):
+    return render(request, 'devs/index.html', { 'devs': devs })
