d451ee34abde22fc20f83c1e7fafe1e0e7600746
Michael Hamilton <hamiltonisaiah9119@gmail.com>
2021-09-19 16:01:10 -0700

    admin for photo

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Dev, Interview, Language
+from .models import Dev, Interview, Language, Photo
 
 admin.site.register(Dev)
 admin.site.register(Interview)
 admin.site.register(Language)
+admin.site.register(Photo)
