cb51e1a0568f85ffa7e07c038e4e2dbada60373d
Michael Hamilton <hamiltonisaiah9119@gmail.com>
2021-09-18 17:41:56 -0700

    add urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('devs/', views.devs_index, name='index'),
     path('devs/<int:dev_id>', views.devs_detail, name='detail'),
     path('devs/create', views.DevCreate.as_view(), name='devs_create'),
+    path('devs/<int:pk>/update', views.DevUpdate.as_view(), name='devs_update'),
+    path('devs/<int:pk>/delete', views.DevDelete.as_view(), name='devs_delete'),
 ]
